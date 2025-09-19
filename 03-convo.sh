27712c3aaa44c3516d5421c7cf23ed6218d19405
Rajkumar <rajkumarmanda@outlook.com>
2025-09-19 16:00:17 +0530

    third Commit

---
+++ 03-convo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Raju
+PERSON2=Pasha
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I'm Good, How about you $PERSON1"
+echo "$PERSON1: I'm good too. what are you learning"
+echo "$PERSON2: I'm Learning Devops with AWS"
