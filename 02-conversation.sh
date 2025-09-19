103d95df076a38ec3fcdc26e80e753cf31937fc7
Rajkumar <rajkumarmanda@outlook.com>
2025-09-19 15:52:31 +0530

    second commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I'm Good, How about you"
+echo "Ramesh: I'm good too. what are you learning"
+echo "Suresh: I'm Learning Devops with AWS"
