738ada8443f0ef6c379564adec41189c96842592
Rajkumar <rajkumarmanda@outlook.com>
2025-09-25 15:21:07 +0530

    practice

--- 06-practice-install.sh
+++ 06-practice-install.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR :: Please run this command in root privilage"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR :: Installing mysql is Failure"
+    exit 1 # failure is other than 0
 else
     echo "installing mysql is SUCCESS"
 fi
