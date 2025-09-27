1be274ed9fe44524e7e65580ede599ca087e65f8
Rajkumar <rajkumarmanda@outlook.com>
2025-09-27 14:57:50 +0530

    practice

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f )
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 USERID=$(id -u)
